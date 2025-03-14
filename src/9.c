f71f5c09c58acdc43637fab7862aa625c003c85a
gigomeck <gigomeck@gmail.com>
2025-03-14 06:36:30 +0000

    Add 9.c

---
+++ src/9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    int y = 10;
+    int z = add(x, y);
+    printf("The sum of %d and %d is %d", x, y, z);
+    return 0;
+}
+int add(int a, int b) {
+    return a + b;
+}
