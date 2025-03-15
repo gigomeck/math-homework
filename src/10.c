f459f70601392f02044dae7fe15a969e829d63af
gigomeck <gigomeck@gmail.com>
2025-03-15 05:22:55 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // Declare variables
+    int a = 5;
+    int b = 7;
+    float c = 3.14;
+    char d = 'A';
+
+    // Print variables using printf
+    printf("a: %d\nb: %d\nc: %f\nd: %c\n", a, b, c, d);
+
+    // Perform some mathematical operations
+    int sum = a + b;
+    float product = c * 2.0;
+
+    // Print the results using printf
+    printf("Sum: %d\nProduct: %f\n", sum, product);
+
+    return 0;
+}
