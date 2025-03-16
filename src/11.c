6112b2a1ffda3c6762fb00f0db74d8dce04602cd
gigomeck <gigomeck@gmail.com>
2025-03-16 19:17:05 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    float a = 3.14;
+    float b = 2.78;
+    float c = a * b;
+
+    printf("The value of c is: %f\n", c);
+
+    return 0;
+}
